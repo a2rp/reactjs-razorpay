b7c788227d162b13683c3fda3af9d79d605d2aa5
ash1198 <ash.ranjan09@gmail.com>
2022-06-14 00:25:52 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [amount, setAmount] = useState("");
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        if (amount==="") {
+            alert ("pls enter amount");
+        }
+        else {
+            // alert (amount);
+            let options = {
+                key: "rzp_test_ONyaLZToaGIma6",
+                key_secret: "r9y7ZS5n6ABbu1P2xD1wljSB",
+                amount: amount * 100,
+                currency: "INR",
+                name: "STARTUP_PROJECTS",
+                description: "for testing purpose",
+                handler: (response) => {
+                    alert (response.razor_payment_id);
+                },
+                prefill: {
+                    name: "Ashish",
+                    email: "ash.ranjan09@gmail.com",
+                    contact: "8123747965",
+                },
+                notes: {
+                    address: "BTM Layout, Bengaluru"
+                },
+                theme: {
+                    color: "#123"
+                }
+            };
+            let pay = new window.Razorpay(options);
+            pay.open();
+        }
+    };
+    return (
+        <div className="App">
+            <h3>Razorpay payment integration with reactjs</h3>
+            <input type="text" placeholder="enter amount" value={amount} onChange={(event)=>setAmount(event.target.value)} /> 
+            <button onClick={handleSubmit}>submit</button>
+        </div>
+    );
 }
 
 export default App;
